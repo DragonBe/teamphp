e02bece403fb99745b6ab0415ce6a8500bd6d44e
Michelangelo <dragonbe+github@gmail.com>
2012-05-25 00:09:34 -0500

    Updating schema default settings

--- docs/Db/schema.mysql.sql
+++ docs/Db/schema.mysql.sql
@@ -1,11 +1,45 @@
 --
--- Account data
+-- Table structure for table `account`
 --
+
 DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `twitterId` INT UNSIGNED NOT NULL,
-    `twitterName` VARCHAR(255) NOT NULL,
-    `token` TEXT NOT NULL,
-    PRIMARY KEY (`id`)
-) Engine=MyIsam CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `twitterId` int(10) unsigned NOT NULL,
+  `twitterName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `changelog`
+--
+
+DROP TABLE IF EXISTS `changelog`;
+CREATE TABLE IF NOT EXISTS `changelog` (
+  `change_number` bigint(20) NOT NULL,
+  `delta_set` varchar(10) NOT NULL,
+  `start_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `complete_dt` timestamp NULL DEFAULT NULL,
+  `applied_by` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE IF NOT EXISTS `session` (
+  `session_id` char(32) NOT NULL,
+  `save_path` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL DEFAULT '',
+  `modified` int(11) DEFAULT NULL,
+  `lifetime` int(11) DEFAULT NULL,
+  `session_data` text,
+  PRIMARY KEY (`session_id`,`save_path`,`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
