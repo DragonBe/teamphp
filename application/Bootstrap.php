7cbfdb4a6c4c85ae3eb9fe52e68483b1bf845387
jack.peterson <jack.peterson@gmail.com>
2012-05-24 21:12:43 -0700

    minor visual 'enhancements'

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,12 @@
 <?php
-
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	public function _initTopNavigation() {
+		$this->bootstrap ( 'layout' );
+		$layout = $this->getResource ( 'layout' );
+		$view = $layout->getView ();
+		$config = new Zend_Config_Xml ( APPLICATION_PATH . '/configs/navigation.xml', 'nav' );
+		$navigation = new Zend_Navigation ( $config );
+		$view->navigation ( $navigation );
+	}
 }
 
---
+++ application/modules/account/controllers/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Account_RegisterController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+
+}
+
