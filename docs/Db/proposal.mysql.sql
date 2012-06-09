ea9a24139fe7b9a06179bd1c48cf06e521a80c44
Michelangelo <dragonbe+github@gmail.com>
2012-06-09 02:32:42 +0200

    Renamed Mydb to teamphp in SQL source

--- docs/Db/proposal.mysql.sql
+++ docs/Db/proposal.mysql.sql
@@ -2,13 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `teamphp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `teamphp` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`account`
+-- Table `teamphp`.`account`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`account` (
+CREATE  TABLE IF NOT EXISTS `teamphp`.`account` (
   `twitterId` BIGINT UNSIGNED NOT NULL ,
   `screenName` VARCHAR(45) NOT NULL ,
   `userName` VARCHAR(150) NOT NULL ,
@@ -19,9 +19,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`subscription`
+-- Table `teamphp`.`subscription`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`subscription` (
+CREATE  TABLE IF NOT EXISTS `teamphp`.`subscription` (
   `parentId` BIGINT UNSIGNED NOT NULL COMMENT 'This is the twitterId for the parent account' ,
   `childId` BIGINT UNSIGNED NOT NULL COMMENT 'This is the twitter id for the child account' ,
   PRIMARY KEY (`parentId`, `childId`) ,
@@ -29,12 +29,12 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`subscription` (
   INDEX `subscription_child_fk` (`childId` ASC) ,
   CONSTRAINT `subscription_parent_fk`
     FOREIGN KEY (`parentId` )
-    REFERENCES `mydb`.`account` (`twitterId` )
+    REFERENCES `teamphp`.`account` (`twitterId` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `subscription_child_fk`
     FOREIGN KEY (`childId` )
-    REFERENCES `mydb`.`account` (`twitterId` )
+    REFERENCES `teamphp`.`account` (`twitterId` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
